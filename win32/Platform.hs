88efc26ddda954c914456f108eb9686f3718d714
qzchenwl <qzchenwl@gmail.com>
2016-03-22 16:27:51 +0800

    'registry'

--- win32/Platform.hs
+++ win32/Platform.hs
@@ -1,8 +1,9 @@
 module Platform where
 
+import System.Win32.Registry
+
 getReg :: String -> String -> IO [(String, String, Int)]
 getReg "HKLM" subkey = do
     hkey <- regCreateKey hKEY_LOCAL_MACHINE subkey
     kvs <- regEnumKeyVals hkey
-    return $ map (\(k, v, type) -> (k, v, fromIntegral type)) kvs
-
+    return $ map (\(k, v, t) -> (k, v, fromIntegral t)) kvs
